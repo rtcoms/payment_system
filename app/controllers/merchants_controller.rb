1468381be91b3e27d25b8819348144507c293e7f
Rohit Trivedi <86.rohit@gmail.com>
2023-10-06 03:29:07 +0530

    linting fixes after setting up ruby-lsp in vscode

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,7 +1,7 @@
 class MerchantsController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
-  before_action :set_merchant, only: %i[ show edit update destroy ]
+  before_action :set_merchant, only: %i[show edit update destroy]
 
   # GET /merchants
   def index
@@ -9,8 +9,7 @@ class MerchantsController < ApplicationController
   end
 
   # GET /merchants/1
-  def show
-  end
+  def show; end
 
   # GET /merchants/new
   def new
@@ -18,15 +17,14 @@ class MerchantsController < ApplicationController
   end
 
   # GET /merchants/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /merchants
   def create
     @merchant = Merchant.new(merchant_params)
 
     if @merchant.save
-      redirect_to @merchant, notice: "Merchant was successfully created."
+      redirect_to @merchant, notice: 'Merchant was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -35,7 +33,7 @@ class MerchantsController < ApplicationController
   # PATCH/PUT /merchants/1
   def update
     if @merchant.update(merchant_params)
-      redirect_to @merchant, notice: "Merchant was successfully updated.", status: :see_other
+      redirect_to @merchant, notice: 'Merchant was successfully updated.', status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -44,17 +42,18 @@ class MerchantsController < ApplicationController
   # DELETE /merchants/1
   def destroy
     @merchant.destroy!
-    redirect_to merchants_url, notice: "Merchant was successfully destroyed.", status: :see_other
+    redirect_to merchants_url, notice: 'Merchant was successfully destroyed.', status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_merchant
-      @merchant = Merchant.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def merchant_params
-      params.require(:merchant).permit(:name, :email, :description, :password, :password_confirmation, :status)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_merchant
+    @merchant = Merchant.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def merchant_params
+    params.require(:merchant).permit(:name, :email, :description, :password, :password_confirmation, :status)
+  end
 end
